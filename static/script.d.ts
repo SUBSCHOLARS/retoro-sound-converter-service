07edab07796577390add6c3f7ba96dd1a2cc3ae0
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-24 22:21:36 +0900

    recompile typescript

--- static/script.d.ts
+++ static/script.d.ts
@@ -1,2 +1,10 @@
-export {};
+declare const targetFsInput: HTMLInputElement;
+declare const fsValDisplay: HTMLSpanElement;
+declare const nBitsInput: HTMLInputElement;
+declare const bitValDisplay: HTMLSpanElement;
+declare const convertForm: HTMLFormElement;
+declare const resultArea: HTMLDivElement;
+declare const audioPlayer: HTMLAudioElement;
+declare const downloadLink: HTMLAnchorElement;
+declare const audioFileInput: HTMLInputElement;
 //# sourceMappingURL=script.d.ts.map
--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -79,5 +80,4 @@ convertForm.addEventListener('submit', (e) => __awaiter(void 0, void 0, void 0, 
         btn.classList.remove('opacity-50', 'cursor-not-allowed');
     }
 }));
-export {};
 //# sourceMappingURL=script.js.map
